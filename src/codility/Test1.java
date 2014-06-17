30a00e77ee24f69634b16fc80bbf80b5c8f62f89
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-06-16 21:37:34 -0500

    Test q impl in java

---
+++ src/codility/Test1.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package codility;
+
+import java.util.Arrays;
+
+/**
+ * @author Dany
+ *
+ */
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] A={10,0,8,2,-1,12,11,3};
+		//int[] A={2};
+		System.out.println(new Test1().solution(A));
+	}
+
+	public int solution(int[] A) {
+		// write your code in Java SE 8
+		int N=A.length;
+		int minValue=Integer.MAX_VALUE, maxValue=Integer.MIN_VALUE;
+		int currentLimitIndex=0, currentMax=0,maxGap=Integer.MIN_VALUE;
+
+		for(int i=0;i<N;i++)
+		{
+			if(A[i]<minValue)
+			{
+				minValue=A[i];
+			}
+			if(A[i]>maxValue)
+			{
+				maxValue=A[i];
+			}
+		}
+		Arrays.sort(A);
+		for(int j=0;j<N;j++)
+		{
+			if(j<=maxValue&&j>=minValue)
+			{
+				if(j<=A[currentLimitIndex])
+				{
+					if(j==0||j==N-1)
+					{
+						currentMax=Math.abs(j-A[currentLimitIndex]);
+						if(currentMax>maxGap)
+						{
+							maxGap=currentMax;
+						}
+					}else 
+					{
+						currentMax=Math.min(Math.abs(A[currentLimitIndex-1]-j), Math.abs(j-A[currentLimitIndex]));
+						if(currentMax>maxGap)
+						{
+							maxGap=currentMax;
+						}
+					}
+				}else
+				{
+					if(currentLimitIndex<N)
+						currentLimitIndex++;
+				}
+			}
+		}
+
+		return maxGap;
+	}
+}
