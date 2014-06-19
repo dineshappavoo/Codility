9a5a09b8219f83a92544af65b9acb562fc9d2a0e
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-06-18 23:44:38 -0500

    Maximum Water flow

---
+++ src/codility/MaximumWaterFlow.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package codility;
+
+/**
+ * @author Dany
+ *
+ */
+public class MaximumWaterFlow {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int[] solution(int[] A)
+	{
+		int[] res=null;
+		
+		
+		
+		return res;
+	}
+
+}
